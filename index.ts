1d2db0879adfe981d76cd6c12617f9c41f737320
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-02-14 00:40:46 +0530

    added agent

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import "dotenv/config";
 import { runLLM } from "./src/llm";
 import { getMessages, addMessages } from "./src/memory.ts";
-
+import { runAgent } from "./src/agent.ts";
+import { z } from "zod";
 const userMessage = process.argv[2];
 
 if (!userMessage) {
   console.error("Please provide a message");
   process.exit(1);
 }
-await addMessages([{ role: "user", content: userMessage }]);
-const messages = await getMessages();
-const response = await runLLM({
-  messages: [...messages, { role: "user", content: userMessage }],
-});
+const weatherTool = {
+  name: "get_stuff",
+  description: "don't use this to get the weather",
+  parameters: z.object({
+    reasoning: z.string().describe("hey why did you pick this tool?"),
+  }),
+};
 
+const response = await runAgent({ userMessage, tools: [weatherTool] });
 console.log(response);
--- src/llm.ts
+++ src/llm.ts
@@ -1,18 +1,25 @@
 import type { AIMessage } from "../types";
 import { openai } from "./ai";
+import { zodFunction } from "openai/helpers/zod";
 export const runLLM = async ({
   model = "gemini-1.5-flash",
   messages,
   temperature = 0.1,
+  tools,
 }: {
   messages: AIMessage[];
   temperature?: number;
   model?: string;
+  tools: any[];
 }) => {
+  const formattedTools = tools.map(zodFunction);
   const response = await openai.chat.completions.create({
     model,
     messages,
     temperature,
+    tools: formattedTools,
+    tool_choice: "auto",
+    parallel_tool_calls: false,
   });
-  return response.choices[0].message.content;
+  return response.choices[0].message;
 };
