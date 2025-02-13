894dd1fa7c98940483b69fff6f80fc6507d0265b
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-02-13 20:56:46 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { runLLM } from "./src/llm";
+import { getMessages } from "./src/memory.ts";
+
+const userMessage = process.argv[2];
+
+if (!userMessage) {
+  console.error("Please provide a message");
+  process.exit(1);
+}
+
+const messages = await getMessages();
+const response = await runLLM({
+  messages: [...messages, { role: "user", content: userMessage }],
+});
+
+console.log(response);
---
+++ src/llm.ts
@@ -0,0 +1,18 @@
+import type { AIMessage } from "../types";
+import { openai } from "./ai";
+export const runLLM = async ({
+  model = "gemini-1.5-flash",
+  messages,
+  temperature = 0.1,
+}: {
+  messages: AIMessage[];
+  temperature?: number;
+  model?: string;
+}) => {
+  const response = await openai.chat.completions.create({
+    model,
+    messages,
+    temperature,
+  });
+  return response.choices[0].message.content;
+};
