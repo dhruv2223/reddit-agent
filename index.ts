109faa0b189781ba9832bde9e3db074d4a8035fa
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-02-13 21:04:18 +0530

    made the chat context aware

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { runLLM } from "./src/llm";
-import { getMessages } from "./src/memory.ts";
+import { getMessages, addMessages } from "./src/memory.ts";
 
 const userMessage = process.argv[2];
 
@@ -8,7 +8,7 @@ if (!userMessage) {
   console.error("Please provide a message");
   process.exit(1);
 }
-
+await addMessages([{ role: "user", content: userMessage }]);
 const messages = await getMessages();
 const response = await runLLM({
   messages: [...messages, { role: "user", content: userMessage }],
